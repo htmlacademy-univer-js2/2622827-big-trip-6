fb1635d962f8b9e4932015ee78cdcd67b2c1cfe9
Valeria Zyryanova <zyryanova-2006@inbox.ru>
2026-04-01 15:19:34 +0500

    4.17. Шаблонизируй то

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,4 +1,5 @@
 import {generatePoint, destinations, offersByType} from '../mock/point.js';
+import {generateFilters} from '../mock/filter.js';
 
 const POINT_COUNT = 5;
 
@@ -19,6 +20,10 @@ export default class PointsModel {
 
   getOffersByType() {
     return this.offersByType;
+  }
+
+  getFilters() {
+    return generateFilters(this.points);
   }
 }
 
